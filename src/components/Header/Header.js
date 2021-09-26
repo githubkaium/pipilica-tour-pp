99d49673e45a1e68b6a437f469b8556d42046dfa
githubkaium <mailtokaium@gmail.com>
2021-09-26 15:17:00 +0600

    first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className="title">PiPiLiCa Tour Member List</h1>
+        </div>
+    );
+};
+
+export default Header;
---
+++ src/components/List/List.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './List.css';
+
+const List = () => {
+    return (
+        <div className="list">
+            <div className="item">
+                <h4>Item Lists</h4>
+            </div>
+            <div className="pick">
+                <h4>Picked Item</h4>
+            </div>
+        </div>
+    );
+};
+
+export default List;
